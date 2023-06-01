7243a4240a7feace3b80e1014b19b1a1a3fb3277
Esteban <esteban.ouvre@gmail.com>
2023-06-01 11:06:49 +0200

    Breadcrumbs add to layout

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Seo title="Monkey's" description="Description..." />
-      <Layout>
+      <Layout isDisplayBreadcrumbs={false}>
         <LandingPageView />
       </Layout>
     </>
--- frontend/src/ui/component/layout/layout.tsx
+++ frontend/src/ui/component/layout/layout.tsx
@@ -1,14 +1,18 @@
+import { Breadcrumbs } from "../breadcrumbs/breadcrumbs";
 import { Footer } from "../navigation/footer";
 import { Navigation } from "../navigation/navigation";
 
 interface Props {
   children: React.ReactNode;
+  isDisplayBreadcrumbs?: boolean;
 }
 
-export const Layout = ({ children }: Props) => {
+export const Layout = ({ children, isDisplayBreadcrumbs = true }: Props) => {
   return (
     <>
       <Navigation />
+      {isDisplayBreadcrumbs && <Breadcrumbs />}
+
       {children}
       <Footer />
     </>
